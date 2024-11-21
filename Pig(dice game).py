b51ce6b330f71ded9e492bf95913487765b98c20
Swaroop <141646466+Swaroop5222@users.noreply.github.com>
2024-11-21 20:32:10 +0530

    Update Pig(dice game).py

--- Pig(dice game).py
+++ Pig(dice game).py
@@ -1,13 +1,4 @@
-#Pig(dice game)
-'''
-Each turn, a player repeatedly rolls a die until the chosen number is 1.
 
-If the player rolls the number 1 at the beginning of the game, they score nothing and it becomes the next player's turn.
-If the player rolls any other number, it is added to their turn total and the player's turn continues.
-If a player rolls number 1, their turn total is added to their score, and it becomes the next player's turn.
-The first player to score 100 or more points wins.
-
-'''
 import random
 
 def user_turn(user_total):
